e72a73cee55523d264e8a319310f81a6660449bb
«Aliaksei <alex_26.89@mail.ru>
2023-04-21 23:22:03 +0300

    Внесены изменения в example003_sum

--- Examples/example003_Sum/Program.cs
+++ Examples/example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 43;
-int numberB = 4;
+﻿int numberA = new Random().Next(1,20);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,30);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
