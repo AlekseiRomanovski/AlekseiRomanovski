93a905f4de852295cc7328b4aa874ac77ea3a783
«Aliaksei <alex_26.89@mail.ru>
2023-04-27 22:27:27 +0300

    Выполнено задание 1

---
+++ C#Lesson1/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число а = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+int b2 = b*b;
+if (a == b2)
+{
+    Console.Write("a является квадратом числа b");
+} 
+else
+{
+    Console.Write("a не является квадратом числа b");
+}
