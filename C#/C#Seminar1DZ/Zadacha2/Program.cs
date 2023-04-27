c816a29c8d70cf78b7865116d63b671e07658b6a
«Aliaksei <alex_26.89@mail.ru>
2023-04-28 01:16:39 +0300

    add Zadacha2

--- C#/C#Seminar1DZ/Zadacha2/Program.cs
+++ C#/C#Seminar1DZ/Zadacha2/Program.cs
@@ -1,6 +1,13 @@
-﻿Console.WriteLine("Введите число a = ");
-int a = Convert.ToInt32(Console.ReadLine());
+﻿/*Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
 Console.WriteLine("Введите число a = ");
 int a = Convert.ToInt32(Console.ReadLine());
-int b = a % 10;
-Console.WriteLine(b);
+Console.WriteLine("Введите число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b) Console.WriteLine("Вы ввели два одинаковых числа");
+else if (a < b) Console.WriteLine("число b больше числа a");
+else Console.WriteLine("число b меньше числа a");
