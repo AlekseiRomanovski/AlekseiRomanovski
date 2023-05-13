4baf4ab779b5186f603f990f2cf9c7393af0f12c
«Aliaksei <alex_26.89@mail.ru>
2023-05-13 17:35:28 +0300

    add task1, task2

---
+++ C#/C#Seminar2/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Написать программу, которая выводит случайное число
+из отрезка [10, 99] и показывает наибольшую цифру числа.
+1. Генерация случайного числа
+2. Найти наибольшую цифру числа
+3. Вывод*/
+ 
+Random rand = new Random();
+int number = rand.Next(10, 100);
+int a = number/10;
+int b = number%10;
+if(a>b)
+{
+Console.WriteLine($"Наше число {number}. {a} > {b}. Результат {a}");
+}
+else if (a<b)
+{
+Console.WriteLine($"Наше число {number}. {b} > {a}. Результат {b}");
+}
+else {
+Console.WriteLine($"Наше число {number}. {a} = {b}.");
+}
