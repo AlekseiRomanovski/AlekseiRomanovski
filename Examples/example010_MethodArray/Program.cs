86c51295238d2a99317eeca1b0746afa1a239add
«Aliaksei <alex_26.89@mail.ru>
2023-05-13 11:27:57 +0300

    add example008, example009, example010

---
+++ Examples/example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array  = {11,21,31,111,15, 18, 61,17,18,19};
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index]==find)
+    {
+        Console.Write("index = ");
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
