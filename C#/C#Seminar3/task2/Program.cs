eef9d82b610d634663d294bbd31a3bb41be866f6
«Aliaksei <alex_26.89@mail.ru>
2023-05-14 19:51:49 +0300

    add task2, task3

---
+++ C#/C#Seminar3/task2/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая по 
+заданному номеру четверти, показывает
+диапозон возможных координат
+точек в этой четверти (х и у)*/
+Console.Write("Введите номер четверти системы кординат ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 1)
+{
+    Console.WriteLine("x > 0 , y > 0");
+}
+else if (a == 2)
+{
+    Console.WriteLine("x < 0 , y > 0");
+}
+else if (a == 3)
+{
+    Console.WriteLine("x < 0 , y < 0");
+}
+else if (a == 4)
+{
+    Console.WriteLine("x > 0 , y < 0");
+}
+else
+{
+    Console.WriteLine("Указан не верный номер четверти");
+}
