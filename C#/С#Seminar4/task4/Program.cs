0f4067c4640803a466618cdcb2d28013c3dd2ca4
«Aliaksei <alex_26.89@mail.ru>
2023-05-21 18:30:53 +0300

    Редактирование task4

--- C#/С#Seminar4/task4/Program.cs
+++ C#/С#Seminar4/task4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу , которая выводит 
+массив из 8 элементов, заполненый
+нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+void FillArray(int[] collection) 
+{
+    int length = collection.Length;
+        for (int i = 0; i < length; i++)
+    {
+        collection[i] = new Random ().Next (0, 2);
+    }
+}
+int [] array = new int [8];
+FillArray(array);
+Console.WriteLine(String.Join(",",array));
